2ecafc75c919915916608e75cf7675cd4f146930
CamilleStocq <95492172+CamilleStocq@users.noreply.github.com>
2025-10-06 13:13:10 +0200

    destroy gameobject when you touch it

--- Assets/#Project/Scripts/Health.cs
+++ Assets/#Project/Scripts/Health.cs
@@ -2,20 +2,20 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
-    public GameObject objetATrouver; 
-
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Ennemy")) 
+        if (collision.gameObject.CompareTag("Ennemy"))
         {
+            Destroy(gameObject);
+
             GameObject[] tousLesObjets = FindObjectsOfType<GameObject>();
 
             foreach (GameObject obj in tousLesObjets)
             {
                 if (obj.CompareTag("Destroy"))
                 {
-                    Destroy(obj); 
-                    return; 
+                    Destroy(obj);
+                    return;
                 }
             }
         }
